e45a4970addc1ad0646b8d1bffb1aa3062b99836
xiaohua2003 <xcai3@ualberta.ca>
2021-08-24 23:17:51 -0600

    create server

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const fs=require('fs');
-const data= fs.readFileSync('./txt/input.txt','utf8');
-const outData=`introduction: ${data}\n created on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', outData)
+//const fs=require('fs');
+//const data= fs.readFileSync('./txt/input.txt','utf8');
+//const outData=`introduction: ${data}\n created on ${Date.now()}`;
+//fs.writeFileSync('./txt/output.txt', outData)
+
+////
+//creating server using http module
+const http=require('http')
+const server=http.createServer((req,res)=>{
+    
+    res.end("hello world")
+})
+server.listen (5000, ()=>{
+    console.log("listening to request on port 8000");
+})
