0a2eab4ffdcf6a69dac685c8b10be42eb1b97a53
xiaohua2003 <xcai3@ualberta.ca>
2021-08-25 23:09:46 -0600

    send back api

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-//const fs=require('fs');
+const fs=require('fs');
 //const data= fs.readFileSync('./txt/input.txt','utf8');
 //const outData=`introduction: ${data}\n created on ${Date.now()}`;
 //fs.writeFileSync('./txt/output.txt', outData)
 
 ////
 //creating server using http module
+const data=fs.readFileSync(`${__dirname}/dev-data/data.json`);
+const dataObject=JSON.parse(data)
 const http=require('http')
 const url=require('url')
 const server=http.createServer((req,res)=>{
@@ -13,13 +15,19 @@ const server=http.createServer((req,res)=>{
         res.end('this is overView')
     } else if (pathName==='/product'){
         res.end("this is ourt product")
-    }else{
+    }else if (pathName==='/api'){
+           res.writeHead(200, {
+               'content-type':'application/json'
+           })
+           res.end(data)
+       
+    } else {
         res.writeHead(404, {
             'Content-type':'text/html'
         })
         res.end('<h1>page not found</h1>')
     }
 })
-server.listen (8002, ()=>{
-    console.log("listening to request on port 8002");
+server.listen (8018, ()=>{
+    console.log("listening to request on port 8006");
 })
